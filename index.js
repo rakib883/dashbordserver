7154094adedd5b7200b27d42b5dbe2cd3b5aebe4
rakib883 <sheikhrakib883@gmail.com>
2025-02-03 21:40:34 +0600

    last

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
-// import user from "./router/userRouter.js";
-import cors from "cors"
-// import { connect } from "./config/dbConfig.js";
-// Load environment variables from the .env file
+import user from "./router/userRouter.js";
+import cors from "cors";
+import { connect } from "./config/dbConfig.js";
+
 dotenv.config();
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-const port = process.env.PORT || 3000;
 
+// Connect to database
+connect();
 
-// databse conect area here start
-// connect()
-
-// app.use("/api",user)
+app.use("/api", user);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get("/test",(req,res)=>{
-   res.send({message:"okk"})
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+
+app.get("/test", (req, res) => {
+  res.send({ message: "OKK" });
 });
+
+// ✅ Instead of app.listen(), export the handler
+export default app;
